53cdbe832b1c4e5a335a123e6fc3e50089c0b797
Antoine Caron <antoine.caron@zenika.com>
2017-10-17 15:10:57 +0200

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Random user generator
+
+Generate random user for https://slashgear.github.io/cloud/#/
+
+````json
+{
+    "id": 12121212123123,
+    "firstName": "Antoine",
+    "lastName": "Caron",
+    "birthDay": "2012-07-14",
+    "position": {
+        "lat": 45.737526,
+        "lon": 4.814719,
+    }
+}
+````
+
+Based on https://randomuser.me/
+
---
+++ index.js
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const fs = require('fs');
+
+const nbUserToGenerate = Number(process.argv[2]) || 100;
+const fileName = 'data.json';
+
+
+const generateSign = () => (Math.random() > 0.5) ? 1: -1;
+const generateLat = () => Math.random() * 90;
+const generateLon = () => Math.random() * 180;
+
+const generateRandomPosition  = () => {
+    return {
+        lat: generateSign() * generateLat(),
+        lon: generateSign() * generateLon()
+    }
+};
+
+const persistedUsers = [];
+
+console.log(`Fetching ${nbUserToGenerate} users`);
+fetch(`https://randomuser.me/api/?results=${nbUserToGenerate}`)
+    .then(res => res.json())
+    .then(res => {
+        res.results.forEach(user => {
+            const persistedUser = {
+                firstName: user.name.first,
+                lastName: user.name.last,
+                position: generateRandomPosition(),
+                birthDay: moment(user.dob).format('l'),
+            };
+            persistedUsers.push(persistedUser);
+
+        });
+
+        fs.writeFile(fileName, JSON.stringify(persistedUsers), function(error) {
+            if (error) {
+                console.error("write error:  " + error.message);
+            } else {
+                console.log("Successful Write");
+            }});
+    });
+
+
+
+
+
